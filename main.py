176d9582bc76377af0abb8384025e1bdae41312b
DarkImpact1 <dwivedimohit2001@gmail.com>
2024-10-09 01:34:45 +0530

    adding features

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from module import ColdEmailGenerator
-# Set the title of the app
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
 st.title("Generate Email for Your Job")
 
 # Input for the user's name
@@ -9,15 +16,36 @@ employer_name = st.text_input("Enter your name").strip().title()
 # Input for the employer's skills
 employer_skills = st.text_input("Enter your skills separated by commas (e.g., skill1, skill2)").strip()
 
-url = st.text_input("Enter the url of page where job is available")
-email_generator = ColdEmailGenerator()
-# Display the entered information (for testing purposes)
+# Input for the URL of the job posting
+url = st.text_input("Enter the URL of the page where the job is available")
+
+# Experience level selection
+experience_level = st.radio("Select your experience level", ('Fresher', 'Experienced'))
+
+# Additional input based on experience level
+if experience_level == 'Experienced':
+    experience = st.text_input("Your experience example: Machine Learning 2 year in xyz company, Full Stack 1 year in ABC Company").strip()
+    experience = f"I have {experience}"
+else:
+    experience = "I am Fresher"  # Default value for freshers
+
+email_generator = ColdEmailGenerator(api_key=api_key)
+
+# Display the entered information and generate email
 if st.button("Generate Email"):
     if employer_name and employer_skills and url:
         skills_list = [skill.strip().title() for skill in employer_skills.split(",")]
         st.write(f"Name: {employer_name}")
         st.write(f"Skills: {', '.join(skills_list)}")
-        generated_email = email_generator.writeEmail(employer_name=employer_name,employer_skills=employer_skills,url=url)
-        st.text_area("Generated Email : ",generated_email)
+        st.write(f"Experience: {experience}")
+
+        # Generate email based on input
+        generated_email = email_generator.writeEmail(
+            employer_name=employer_name,
+            employer_skills=employer_skills,
+            url=url,
+            employer_experience=experience
+        )
+        st.code(generated_email, "English")
     else:
-        st.error("Please Enter name, skills and url")
+        st.error("Please enter name, skills, and URL")
--- module.py
+++ module.py
@@ -6,19 +6,23 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 
 load_dotenv()
-os.getenv("GROQ_API_KEY")
+
+api_key = os.getenv("GROQ_API_KEY")
 
 class ColdEmailGenerator:
-    def __init__(self,model = "mixtral-8x7b-32768"):
+    def __init__(self,model = "llama-3.1-70b-versatile",api_key = None):
         self.model = model
-        self.llm = ChatGroq(
-            model=self.model,
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-            # other params...
-        )
+        if api_key != None:
+            self.llm = ChatGroq(
+                model=self.model,
+                temperature=0,
+                max_tokens=None,
+                timeout=None,
+                max_retries=2,
+                groq_api_key = api_key
+            )
+        else:
+            print("If you don't have api key just go to\nhttps://console.groq.com/login\nRegister and Generate api key ")
 
 
     def scrapeWebpage(self,url):
@@ -32,50 +36,73 @@ class ColdEmailGenerator:
                     Here is the page response from which you have to extract the job role, experience, skills, and description required for this job in JSON format. 
                     Make sure to only extract the information specifically requested, and there should be NO PREAMBLE.
                     The keys in the output should be 'job role', 'experience', 'skills', and 'description'.
+
+                    IF PAGE DOES NOT CONTAIN ANY JOB POSTING THEN SIMPLY RETURN "None"
                     ### RETURN VALID OUTPUT IN JSON FORMAT
                     '''
                 )
         chain_extract = extraction_prompt | self.llm
         response = chain_extract.invoke(input={'page_content':docs})
-        parser = JsonOutputParser()
-        json_response = parser.parse(response.content)
-        return json_response
+        if response != None:
+            parser = JsonOutputParser()
+            json_response = parser.parse(response.content)
+            return json_response
+        return None
     
 
-    def writeEmail(self,employer_name,employer_skills,url):
+    def writeEmail(self,employer_name,employer_skills,employer_experience,url):
         json_response = self.scrapeWebpage(url)
+        if json_response['job role'] == None:
+            return f"Sorry could find job on that link"
         job_role = json_response['job role']
         experience_required = json_response['experience']
         required_skills = json_response['skills']
         job_description = json_response['description']
-        cold_email_tempelate = PromptTemplate.from_template(
+        cold_email_template = PromptTemplate.from_template(
             '''
-                ### JOB DESCRIPTION
-                {job_description}
-                and skills required 
-                {experience_required}
-                ### INSTRUCTION
-                My name is {name} and I have gained knowledge in {skills} and want to apply for {job_role} which required {required_skills} these skills. 
-                your job is to Generate a professional and personalized cold email to the hiring manager for the job described above and make sure you mentions the the necessary skills from my 
-                skillset which is required for this job role. The email should convey enthusiasm, professionalism, and the candidate's desire to contribute to the 
-                company’s mission. The tone should be respectful and focused on how the candidate can add value to the team.
-                Also, include a call to action for scheduling an interview or further discussion. The email should be around 150-200 words.
+                ### JOB DETAILS
+                - **Job Description:** {job_description}
+                - **Experience Required:** {experience_required}
+                - **Skills Required:** {required_skills}
 
-                ### NO PREAMBLE
+                ### EMAIL GENERATION INSTRUCTIONS
+                My name is {name}, and I have acquired significant expertise in {skills} and {experience}. I am eager to apply for the role of {job_role},
+
+            
+                Your task is to generate a concise, professional cold email to the hiring manager for the job described above. Make sure the email highlights my skills that match the job requirements, 
+                emphasizing my enthusiasm for the role and how my experience can add value to the company’s mission. The tone should be polite, enthusiastic, and solution-oriented, focusing on what I can contribute to the team. 
+                
+                Remember If My experience is not aligned with experience required then don't add experience
+
+                Conclude with a call to action for scheduling an interview or further discussion. The email should be succinct (around 200-250 words), polished, and ready to be copied and pasted.
+
+                ### NO PREAMBLE, JUST EMAIL CONTENT
             '''
-            )
-        email_chain = cold_email_tempelate | self.llm
-        final_email = email_chain.invoke(input = {'job_description':job_description,'experience_required':experience_required,'name': employer_name, 'skills':employer_skills,'job_role':job_role,'required_skills':required_skills})
+        )
+
+        email_chain = cold_email_template | self.llm
+        input = {
+            'job_description':job_description,
+            'experience_required':experience_required,
+            'required_skills':required_skills,
+            'name': employer_name,
+            'skills':employer_skills,
+            'experience':employer_experience,
+            'job_role':job_role,
+        }
+        final_email = email_chain.invoke(input = input )
 
         return final_email.content
 
 
 def main():
-    generator = ColdEmailGenerator()
-    url = "https://www.google.com/about/careers/applications/jobs/results/136260588966683334-software-engineer-ii-full-stack-youtube"
+    generator = ColdEmailGenerator(api_key=api_key)
+    url = "https://pixabay.com/music/"
     name = "mohit Dwivedi"
     skills= ['Python','Machine Learning','Deep learning','AI and ML','Mongo DB','React Native','Full Stack']
-    response = generator.writeEmail(name,skills,url)
+    exp = "Fresher"
+    response = generator.writeEmail(name,skills,exp,url)
+    # response = generator.scrapeWebpage(url)
     print(response)
 
 
