3e85ee0bdef91d3cfc978bd54ab7ba92ed12028c
Mohit Dwivedi <mohit.dev.new@gmail.com>
2024-10-15 01:29:35 +0530

    Update models

--- main.py
+++ main.py
@@ -8,51 +8,68 @@ load_dotenv()
 
 api_key = os.getenv("GROQ_API_KEY")
 
-st.title("Generate Email for Your Job")
-st.write("""
-Welcome! In this project, you will be asked to provide your full name, along with your skills and experience (optional).
-Based on this information, a personalized cold email will be generated, which you can copy and send to hiring managers for job opportunities.
-Simply paste the job posting URL, and let the tool handle the rest. Sit back and relax!
-         
-""")
 
 
-# Input for the user's name
-employer_name = st.text_input("Enter your name").strip().title()
+def generateEmail(model,employer_name="User",employer_skills="beginner",url="https://www.google.com",experience="I am fresher"):
+    skills_list = [skill.strip().title() for skill in employer_skills.split(",")]
+    # Generate email based on input
+    generated_email = model.writeEmail(
+        employer_name=employer_name,
+        employer_skills=employer_skills,
+        url=url,
+        employer_experience=experience
+    )
+    st.text_area("Generated email", generated_email,height=400)
+    return [True,generated_email]
 
-# Input for the employer's skills
-employer_skills = st.text_input("Enter your skills separated by commas (e.g., skill1, skill2)").strip()
+def main():
+    st.title("Generate Email for Your Job")
+    st.write("""
+    Welcome! In this project, you will be asked to provide your full name, along with your skills and experience (optional).
+    Based on this information, a personalized cold email will be generated, which you can copy and send to hiring managers for job opportunities.
+    Simply paste the job posting URL, and let the tool handle the rest. Sit back and relax!
+            
+    """)
 
-# Input for the URL of the job posting
-url = st.text_input("Enter the URL of the page where the job is available")
+    # List of models for the drop-down
+    models = [
+        "gemma-7b-it", "gemma2-9b-it",
+        "llama3-groq-70b-8192-tool-use-preview", "llama3-groq-8b-8192-tool-use-preview",
+        "distil-whisper-large-v3-en",
+        "llama-3.1-70b-versatile", "llama-3.1-8b-instant",
+        "llama-3.2-11b-text-preview", "llama-3.2-11b-vision-preview",
+        "llama-3.2-1b-preview", "llama-3.2-3b-preview",
+        "mixtral-8x7b-32768",
+        "whisper-large-v3", "whisper-large-v3-turbo",
+        "llava-v1.5-7b-4096-preview"
+    ]
+    # Input for the user's name
+    employer_name = st.text_input("Enter your name").strip().title()
 
-# Experience level selection
-experience_level = st.radio("Select your experience level", ('Fresher', 'Experienced'))
+    # Input for the employer's skills
+    employer_skills = st.text_input("Enter your skills separated by commas (e.g., skill1, skill2)").strip()
 
-# Additional input based on experience level
-if experience_level == 'Experienced':
-    experience = st.text_input("Your experience example: Machine Learning 2 year in xyz company, Full Stack 1 year in ABC Company").strip()
-    experience = f"I have {experience}"
-else:
-    experience = "I am Fresher"  # Default value for freshers
+    # Input for the URL of the job posting
+    url = st.text_input("Enter the URL of the page where the job is available")
 
-email_generator = ColdEmailGenerator(api_key=api_key)
+    # Experience level selection
+    experience_level = st.radio("Select your experience level", ('Fresher', 'Experienced'))
 
-# Display the entered information and generate email
-if st.button("Generate Email"):
-    if employer_name and employer_skills and url:
-        skills_list = [skill.strip().title() for skill in employer_skills.split(",")]
-        st.write(f"Name: {employer_name}")
-        st.write(f"Skills: {', '.join(skills_list)}")
-        st.write(f"Experience: {experience}")
-
-        # Generate email based on input
-        generated_email = email_generator.writeEmail(
-            employer_name=employer_name,
-            employer_skills=employer_skills,
-            url=url,
-            employer_experience=experience
-        )
-        st.code(generated_email, "English")
+    # Additional input based on experience level
+    if experience_level == 'Experienced':
+        experience = st.text_input("Your experience example: Machine Learning 2 year in xyz company, Full Stack 1 year in ABC Company").strip()
+        experience = f"I have {experience}"
     else:
-        st.error("Please enter name, skills, and URL")
+        experience = "I am Fresher"  # Default value for freshers
+    # Create a drop-down list for model selection
+    selected_model = st.selectbox("Select a model", models)
+    email_generator = ColdEmailGenerator(api_key=api_key,model=selected_model)
+    # Display the entered information and generate email
+    if st.button("Generate Email"):
+        if employer_name and employer_skills and url:
+            generateEmail(email_generator,employer_name,employer_skills,url,experience)
+        else:
+            st.error("Please enter name, skills, and URL")
+
+if __name__ == "__main__":
+    main()
