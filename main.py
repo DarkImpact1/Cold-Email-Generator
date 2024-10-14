4a919a70ce19d6bcbd82718f9635bc7424a2b87f
Mohit Dwivedi <mohit.dev.new@gmail.com>
2024-10-15 01:51:50 +0530

    Update models

--- main.py
+++ main.py
@@ -33,13 +33,14 @@ def main():
 
     # List of models for the drop-down
     models = [
+        "mixtral-8x7b-32768",
         "gemma-7b-it", "gemma2-9b-it",
         "llama3-groq-70b-8192-tool-use-preview", "llama3-groq-8b-8192-tool-use-preview",
         "distil-whisper-large-v3-en",
         "llama-3.1-70b-versatile", "llama-3.1-8b-instant",
         "llama-3.2-11b-text-preview",
         "llama-3.2-1b-preview", "llama-3.2-3b-preview",
-        "mixtral-8x7b-32768",
+        
     ]
     # Input for the user's name
     employer_name = st.text_input("Enter your name").strip().title()
